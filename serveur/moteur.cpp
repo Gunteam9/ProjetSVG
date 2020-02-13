a4919062ebd1c95daa1590235063e7d57553e912
Samir Toularhmine <samir.toularhmine@etu.univ-orleans.fr>
2020-02-14 00:29:53 +0100

    Avancées sur la modification du svg

--- serveur/moteur.cpp
+++ serveur/moteur.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[]){
   Window w(500, 500, "Moteur de rendu SVG"); 
   w.init(&argc, &argv);
   w.start();
+  Message m("test", "1");
+  w.update({m});
   std::cout << "hello" << std::endl;
   w.stop();
 
