6f51cedd4a2d5285156037273eed539a5b58f371
IonasNeonakis <ionas.neonakis@gmail.com>
2020-02-16 18:54:47 +0100

    Test pour romain corbeau

--- serveur/maintest.cpp
+++ serveur/maintest.cpp
@@ -4,17 +4,24 @@
 
 
 
-/*
 
-#include "include/serveur.hpp"
 
+
+
+#include "include/dataparser.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    Serveur s;
-    s.startServer();
-}*/
+    DataParser p ;
+    cbor::map map = {{"jeuis", "une map"},{"jesuis2","une deuxime valeur"}};
+    cbor::binary encoded  = cbor::encode(map);
+
+    vector<Message> m = p.lireMessage(encoded);
+    cout <<m[0] <<endl;
+
+    //cout <<m[1]<<endl;
+}
 
 
 
