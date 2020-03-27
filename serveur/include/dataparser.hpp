7efc8e8dbbe6be0a7234f47f40cabd5f6a243833
Samir Toularhmine <samir.toularhmine@gmail.com>
2020-03-27 21:04:54 +0100

    Début de la documentation

--- serveur/include/dataparser.hpp
+++ serveur/include/dataparser.hpp
@@ -4,45 +4,80 @@
 
 #pragma once
 
-#include "message.hpp"
-#include <stdlib.h>
 #include <map>
+#include <stdlib.h>
+
 #include "../../vendor/cbor11/cbor11.hpp"
 #include "../../vendor/exceptions/errorException.hpp"
+#include "message.hpp"
 #include "window.hpp"
 
+/**
+ * @brief Cette map associe un mot clé couleur à une valeur héxadécimale. 
+ * 
+ */
 static std::map<std::string, std::string> COULEURS_MAP;
 
 class DataParser {
-public:
-    const map<const char *, const char *> &getLesElementsDriven() const;
+    private:
+        std::map<const char *,const  char*> lesElementsDriven;
 
-    DataParser();
+        /**
+         * @brief Instance de la classe DataParser, c'est un singleton.
+         * 
+         */
+        static DataParser instance;
 
-private:
-     std::map<const char *,const  char*> lesElementsDriven;
+        /**
+         * @brief Initialisation de la map de couleurs.
+         * 
+         */
+        void initColorMap();
 
-     static DataParser instance;
+        /**
+         * @brief Cette map associe un type d'attribut SVG à une fonction d'interpolation.
+         * Cette fonction prend en paramètre la valeur actuelle, la nouvelle valeur ainsi que le nombre 
+         * de pas à effectuer pour passer de l'ancienne à la nouvelle valeur.
+         * 
+         */
+        std::map<std::string, std::vector<std::string>(*)(std::string, std::string, double)> interpolationMap;
 
-     void initColorMap();
+    public:
+        DataParser();
 
-     std::map<std::string, std::vector<std::string>(*)(std::string, std::string, double)> interpolationMap;
+        const map<const char *, const char *> &getLesElementsDriven() const;
 
-public:
-     static DataParser& getInstance();
+        static DataParser& getInstance();
 
-     vector<Message> lireMessage(cbor::binary);
+        /**
+         * @brief IONAS TODO
+         * 
+         * @return vector<Message> 
+         */
+        vector<Message> lireMessage(cbor::binary);
 
-     bool validateValue(const char*, const char*);
+        /**
+         * @brief Cette fonction valide en fonction d'un type d'attribut ainsi que la valeur à tester, l'interpolation ou non.
+         * Elle va appliquer la regex du type d'attribut à la valeur et décider ou non si le message sera traité ou non.
+         * 
+         * @return true si la valeur est éligible au traitement
+         * @return false si la valeur n'est pas éligible au traitement
+         */
+        bool validateValue(const char*, const char*);
 
-     std::vector<std::string> interpolate(std::string, std::string, std::string, double);
+        /**
+         * @brief Cette fonction effectue l'interpolation en fonction de la première valeur et la dernière valeur.
+         * 
+         * @return std::vector<std::string> 
+         */
+        std::vector<std::string> interpolate(std::string, std::string, std::string, double);
 
-    cbor::map getCss();
+        cbor::map getCss();
 
-    void setLesElementsDriven(const map<const char *, const char *> &lesElementsDriven);
+        void setLesElementsDriven(const map<const char *, const char *> &lesElementsDriven);
 
-    std::map<string,std::map<string,string>> getCssValues();
+        std::map<string,std::map<string,string>> getCssValues();
 
-    map<string, std::map<string, string>> getCssValuesReceived();
+        map<string, std::map<string, string>> getCssValuesReceived();
 };
 
